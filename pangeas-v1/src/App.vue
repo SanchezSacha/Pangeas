4067aedb6e50a7e467339149f0f06d82e4c7cdba
Sacha_Sanchez <amiralpingouin@gmail.com>
2025-06-13 17:21:06 +0200

    Récupération des données des lieux et ajout du composant MapLeaflet - V1.2.0

--- pangeas-v1/src/App.vue
+++ pangeas-v1/src/App.vue
@@ -1,11 +1,30 @@
-<script setup>
-</script>
-
 <template>
-  <div>
+  <div class="container-fluid p-0">
+    <MapLeaflet :places="places" />
   </div>
-
 </template>
 
-<style scoped>
-</style>
+<script>
+import axios from 'axios';
+import MapLeaflet from "./components/MapLeaflet.vue";
+
+export default {
+  name: 'App',
+  components: {
+    MapLeaflet
+  },
+  data() {
+    return {
+      places: []
+    };
+  },
+  async mounted() {
+    try {
+      const res = await axios.get('http://localhost:3000/accueil');
+      this.places = res.data;
+    } catch (err) {
+      console.error('Erreur lors du chargement des lieux :', err.message);
+    }
+  }
+};
+</script>
