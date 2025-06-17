c7be70003515df4ac59fa6545fae8aa597249d88
Sacha_Sanchez <amiralpingouin@gmail.com>
2025-06-17 11:57:59 +0200

    Personnalisation de la popup du lieu au clique du point d'intérêt - V1.2.1

--- pangeas-v1/src/main.js
+++ pangeas-v1/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import '../src/assets/css/map.css'
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue'
